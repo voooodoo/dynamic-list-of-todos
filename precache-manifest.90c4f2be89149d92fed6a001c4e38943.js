b6a92afb5044c44fe77f35b6cb0ff06f02b93dec
ast_yzakharevych <y.zakharevych@astoundcommerce.com>
2019-06-27 10:35:00 +0300

    Updates

---
+++ precache-manifest.90c4f2be89149d92fed6a001c4e38943.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a65e18ce405127512d2763c24587af",
+    "url": "/dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "360950f93242bbe8df63",
+    "url": "/dynamic-list-of-todos/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/dynamic-list-of-todos/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "360950f93242bbe8df63",
+    "url": "/dynamic-list-of-todos/static/js/main.04189027.chunk.js"
+  },
+  {
+    "revision": "3662163eed76d485098b",
+    "url": "/dynamic-list-of-todos/static/js/runtime~main.dcf88686.js"
+  }
+]);
---
+++ static/js/main.04189027.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{10:function(e,n,t){"use strict";t.r(n);var o=t(0),a=t.n(o),c=t(2),r=t.n(c);t(9);var i=function(){return a.a.createElement("div",{className:"App"},a.a.createElement("h1",null,"dynamic list of todos"))};r.a.render(a.a.createElement(i,null),document.getElementById("root"))},3:function(e,n,t){e.exports=t(10)},9:function(e,n,t){}},[[3,1,2]]]);
+//# sourceMappingURL=main.04189027.chunk.js.map
